bc39a79e9f6e8d36421cf7037dcee8e874734393
Adrian Rossouw <adrian@developmentseed.org>
2011-03-24 03:42:40 +0200

    remove duplicate title on pages.

--- pages.js
+++ pages.js
@@ -132,7 +132,6 @@ app.get('/user', function(req, res, next) {
     if (req.session.user) {
         res.render(view('content'), {locals: {
             pageTitle: req.session.user.name,
-            title: req.session.user.name,
             content: '<a href="/logout">Log out</a>'
         }});
     }
